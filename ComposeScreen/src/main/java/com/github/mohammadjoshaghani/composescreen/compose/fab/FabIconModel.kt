67377c13ac0d421ecb3b56bc8f8f72cb98cbd925
mohammad <joshaghani.dev@gmail.com>
2025-12-17 12:26:01 +0330

    Fix ImageVector

--- ComposeScreen/src/main/java/com/github/mohammadjoshaghani/composescreen/compose/fab/FabIconModel.kt
+++ ComposeScreen/src/main/java/com/github/mohammadjoshaghani/composescreen/compose/fab/FabIconModel.kt
@@ -1,9 +1,11 @@
 package com.github.mohammadjoshaghani.composescreen.compose.fab
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 
 data class FabIconModel(
     val iconResource: Int? = null,
-    val iconVector: Int? = null,
+    val iconVector: ImageVector? = null,
     val title: String? = null,
     val onFabPressed: () -> Unit,
 )
